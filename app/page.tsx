edf1d97e401519983a33437670a88e2735ee3c00
Mohammadreza Ahdiyan <mohammadreza.ahadiyan@gmail.com>
2026-06-30 12:31:22 +0330

    Restore to corret one

--- app/page.tsx
+++ app/page.tsx
@@ -37,9 +37,6 @@ const Home = () => {
     const footerElement = footerRef.current;
     const galleryElement = galleryRef.current;
 
-    const isMobile = window.innerWidth < 768;
-    const isTablet = window.innerWidth >= 768 && window.innerWidth < 1024;
-
     gsap.set(headerElement, { opacity: 1, yPercent: 0 });
     const nameRect = nameElement.getBoundingClientRect();
     const logoRect = headerLogoElement.getBoundingClientRect();
@@ -49,15 +46,7 @@ const Home = () => {
     const deltaY = logoRect.top - nameRect.top;
     const scaleRatio = logoRect.width / nameRect.width;
 
-    const penStartY = isMobile ? 20 : 40;
-    const penEndXPercent = isMobile ? -60 : -100;
-    const penEndYPercent = isMobile ? -10 : -20;
-    const penFlipX = isMobile ? 20 : isTablet ? 35 : 50;
-    const penFlipY = isMobile ? 40 : 46;
-    const penFlipScale = isMobile ? 0.35 : isTablet ? 0.42 : 0.5;
-    const textExitX = isMobile ? 150 : 300;
-
-    gsap.set(penElement, { yPercent: penStartY });
+    gsap.set(penElement, { yPercent: 40 });
     gsap.set(footerElement, { opacity: 0, yPercent: 100 });
     gsap.set(welcomeElement, { opacity: 0, yPercent: 30 });
     gsap.set(nameElement, { opacity: 0, yPercent: 30 });
@@ -77,24 +66,18 @@ const Home = () => {
     tl.to(penElement, {
       rotate: 10,
       scale: 0.8,
-      xPercent: penEndXPercent,
-      yPercent: penEndYPercent,
+      xPercent: -100,
+      yPercent: -20,
       duration: 2,
     });
-    tl.to(penTextElement, { xPercent: textExitX, duration: 2 }, "<");
+    tl.to(penTextElement, { xPercent: 300, duration: 2 });
 
     tl.to(welcomeElement, { opacity: 1, yPercent: 0, duration: 1 }, "-=0.5");
     tl.to(nameElement, { opacity: 1, yPercent: 0, duration: 1 }, "<+=0.3");
 
     tl.to(
       penElement,
-      {
-        rotate: 71,
-        scale: penFlipScale,
-        xPercent: penFlipX,
-        yPercent: penFlipY,
-        duration: 3,
-      },
+      { rotate: 71, scale: 0.5, xPercent: 50, yPercent: 46, duration: 3 },
       "<-=0.8",
     );
 
@@ -119,7 +102,7 @@ const Home = () => {
   });
 
   return (
-    <main className="flex flex-col bg-white h-dvh w-full overflow-hidden relative">
+    <main className="flex flex-col bg-white h-screen w-full overflow-hidden relative">
       <HeaderComponent headerRef={headerRef} headerLogoRef={headerLogoRef} />
 
       <HomeComponent
