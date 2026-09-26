4fdc521d5e2130e6809569ec42f57f0a50240215
Mohammadreza <mohammadreza.ahadiyan@gmail.com>
2026-09-26 13:16:44 +0330

    fix: pass no-overflow navigation option to slider

--- components/Slider/ReusableSlider.tsx
+++ components/Slider/ReusableSlider.tsx
@@ -98,6 +98,7 @@ export function ReusableSlider<TItem>({
       enabled: !!edgeOverflow?.enabled,
       distance: edgeOverflow?.chargeWheelDistance,
       releaseDelay: edgeOverflow?.releaseDelay,
+      allowWithoutOverflow: allowEdgeWithoutOverflow,
       onCommit: ({ direction, progress }) =>
         edgeOverflow?.onCommit?.({ direction, progress, activeIndex }),
     },
