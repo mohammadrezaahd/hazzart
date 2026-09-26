58c46bf2f90048718f8fe751377729df0a3b80cd
Mohammadreza <mohammadreza.ahadiyan@gmail.com>
2026-09-26 15:28:28 +0330

    feat: add contact page and Figma social icons

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+import { ContactExperience } from '@/components/Contact/ContactExperience';
+
+export const metadata = { title: 'Contact' };
+
+export default function ContactPage() {
+  return <ContactExperience />;
+}
